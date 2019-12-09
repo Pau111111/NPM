763e80d6f46704b6957270c2ad39d6bc4987ac4f
diegosilva91 <diegoarturosilva@hotmail.com>
2019-12-09 11:00:03 +0100

    moment.js index.js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import * as user from './moment.js'; // not {User}, just User
+let User = user.default;// the default export
+new User('Diego');
---
+++ src/moment.js
@@ -0,0 +1,8 @@
+export default class User { // just add "default"
+  constructor(name) {
+    this.name = name;
+  }
+}
+export function sayHi(user) {
+    alert(`Hello, ${user}!`);
+  }
