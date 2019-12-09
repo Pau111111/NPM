4b1fc6d3a0e7b7456e9ca2db3a94af73197d8621
diegosilva91 <diegoarturosilva@hotmail.com>
2019-12-09 17:03:02 +0100

    npm test 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {User, sayHi} from './moment.js'; // not {User}, just User
+import {sayHi} from './moment.js'; // not {User}, just User
 //let User = user.default;// the default export
-new User('Diego');
-console.log(User);
+//new User('Diego');
+//sconsole.log(User);
 console.log(sayHi('Diego'));
--- src/moment.js
+++ src/moment.js
@@ -1,8 +1,8 @@
-export default class User { // just add "default"
+/*export default class User { // just add "default"
   constructor(name) {
     this.name = name;
   }
-}
+}*/
 export function sayHi(user) {
     alert(`Hello, ${user}!`);
   }
